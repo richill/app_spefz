a78f4f881d144b0f0c952be1186804c7227c9bbd
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-07-02 15:07:17 +0100

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def hello
-    render text: "hello, I am the Social App!"
+  def after_sign_in_path_for(resources)
+    if user_signed_in?
+      user_path(current_user)
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :signups
 
   devise_for :users
-  #root 'application#hello'
   
   root 'static_pages#homepg'
   get     'landing-page',                   to: 'static_pages#landingpg'
