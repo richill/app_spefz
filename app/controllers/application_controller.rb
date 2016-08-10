f69311cd9e7d7479f07b993f8d688eb6efdf5ec9
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-08-10 15:52:41 +0100

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resources)
+    events_path
+  end
+  
   def after_sign_in_path_for(resources)
-    user_path(current_user)
+    events_path
   end
 
   def after_sign_out_path_for(resources)
