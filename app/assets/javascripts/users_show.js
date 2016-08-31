ef6d7519cde150374144710372a685268049f373
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-08-31 16:06:59 +0100

    updated

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -2,3 +2,23 @@
     Section: profile page
     File: views / users / _show.html.erb
   =========================================*/
+
+$(document).ready(function() {
+  $('.evts_feed').click(function() {
+    $(".evts_feed_content").show();
+    $(".evts_upcoming_content").hide();
+    $(".evts_past_content").hide();
+  });
+
+  $('.evts_upcoming').click(function() {
+    $(".evts_upcoming_content").show();
+    $(".evts_feed_content").hide();
+    $(".evts_past_content").hide();
+  });
+
+  $('.evts_past').click(function() {
+    $(".evts_past_content").show();
+    $(".evts_feed_content").hide();
+    $(".evts_upcoming_content").hide();
+  });
+});
