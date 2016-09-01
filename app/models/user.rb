ee4a1ba0b067dacd32c6331f09fe321d65fd2bd5
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-01 13:50:02 +0100

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,23 @@ class User < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :socials, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
 
   mount_uploader :image, ImageUploader
+
+  #follow a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #unfollow a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  #returns true is user is following a user
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
 
