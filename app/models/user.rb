f9eb5b52e787a64a55298f9d08c64f3271fe29f7
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-01 13:39:53 +0100

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :socials, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   mount_uploader :image, ImageUploader
 end
