35eec57502f327f8779ae6b09e8429a7b667a2f2
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-06 01:01:50 +0100

    updated

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
+  before_filter :user_not_allowed_to_view, only: [:homepg]
+
 
   def homepg
   end
 
   def landingpg
     @signup = Signup.new
+  end
+
+  def user_not_allowed_to_view
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   resources :signups
   resources :relationships, only: [:create, :destroy]
 
-  devise_for :users
-  resources :users do
-    resources :socials
-  end
+  # devise_for :users
+  # resources :users do
+  #   resources :socials
+  # end
   
-  root 'static_pages#homepg'
-  get  'landing-page',                   to: 'static_pages#landingpg'
+  # root 'static_pages#homepg'
+  root 'static_pages#landingpg'
+  # get  'landing-page',                   to: 'static_pages#landingpg'
+  get  'home-page',                   to: 'static_pages#homepg'
 end
 
