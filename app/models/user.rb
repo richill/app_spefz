9d7cd1af9f5b5dcad29cc296ec98ce8dd68f3178
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-07 16:52:09 +0100

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,16 @@ class User < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :socials, dependent: :destroy
 
+  #users main_user is following / main_user here is follower_id - main_user is the followed_id
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   
+  #users following main_user / main_user here is followed_id - other_user is follower_id
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
-
-  has_many :active_friendships, class_name: "Friendship", foreign_key: "friender_id", dependent: :destroy
-  #has_many :followings, through: :active_friendships, source: :friended
+  has_many :active_friend_requests, class_name: "Friendship", foreign_key: "friender_id", dependent: :destroy
+  has_many :requested_friendships, through: :active_friend_requests, source: :friended
 
 
 
