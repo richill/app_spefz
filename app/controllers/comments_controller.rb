40f67785f07a8ab24364349e4287973faca481e2
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-26 17:04:32 +0100

    updated

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,70 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_filter :load_commentable
+
+  def index
+    @comments = @commentable.comments
+  end
+
+  def show
+  end
+
+  def new
+    @comment = @commentable.comments.new
+  end
+
+  def edit
+  end
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    @social = Social.find(params[:social_id])
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to [@commentable], notice: 'Comment was successfully posted.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { redirect_to social_path(@social), alert: 'Comment was not posted.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to [@commentable], notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :social_id)
+    end
+
+    def load_commentable
+      resource, id = request.path.split('/')[1, 2]
+      @commentable = resource.singularize.classify.constantize.find(id)
+    end
+end
