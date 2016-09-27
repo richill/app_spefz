36438b4bfe206f02dcf9a26d361ae40b079924c9
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-27 11:43:32 +0100

    updated

--- app/controllers/favourite_socials_controller.rb
+++ app/controllers/favourite_socials_controller.rb
@@ -1,26 +1,23 @@
 class FavouriteSocialsController < ApplicationController
-end
-
-
-class FavouriteCompaniesController < ApplicationController
-  before_action :set_company
+  before_action :set_social
   
   def create
-    if Favourite.create(favourited: @company, userj: current_userj)
-      redirect_to @company, notice: 'Company has been Saved'
+    if Favourite.create(favourited: @social, user: current_user)
+      redirect_to @social, notice: 'Social has been Saved'
     else
-      redirect_to @company, alert: 'Something went wrong...*sad panda*'
+      redirect_to @social, alert: 'Something went wrong...*sad panda*'
     end
   end
   
   def destroy
-    Favourite.where(favourited_id: @company.id, userj_id: current_userj.id).first.destroy
-    redirect_to @company, notice: 'Company has been removed from Saved List'
+    Favourite.where(favourited_id: @social.id, user_id: current_user.id).first.destroy
+    redirect_to @social, notice: 'Social has been removed from Saved List'
   end
   
   private
-  
-  def set_company
-    @company = Company.find(params[:company_id] || params[:id])
+  def set_social
+    @social = Social.find(params[:social_id] || params[:id])
   end
 end
+
+
