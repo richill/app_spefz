42fce6c1cc0162e6cb6a8af09bb61da287744018
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-09-27 16:04:30 +0100

    updated

---
+++ app/assets/javascripts/socials_form.js
@@ -0,0 +1,24 @@
+/*=========================================
+    Section: socials page
+    File: views / socials / _form.html.erb
+  =========================================*/
+
+// filter on gender attendance
+$(document).ready(function() {
+  // $('.other_option_content').hide();
+  $('#social_category_quantitygender_id').change(function() {
+    if ($(this).val() == 4) {
+      $('.specific_gender_quantity_content').show();
+      $('.general_gender_quantity_content').hide();
+    } else if ($(this).val() == 1, 2, 3) {
+      $('.general_gender_quantity_content').show();
+      $('.specific_gender_quantity_content').hide();
+    }
+  });
+});
+
+
+
+
+
+
