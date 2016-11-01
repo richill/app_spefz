c807da9e55fe6afe62c4738853abf95b9d089031
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-11-01 21:52:55 +0000

    updated

--- app/models/category_country.rb
+++ app/models/category_country.rb
@@ -1,6 +1,5 @@
 class CategoryCountry < ActiveRecord::Base
   has_many :socials
   has_many :events
-  has_many :category_topics
-  # has_many :category_topics, through: :socials
+  has_many :category_topics, through: :socials
 end
--- app/models/category_topic.rb
+++ app/models/category_topic.rb
@@ -1,5 +1,5 @@
 class CategoryTopic < ActiveRecord::Base
   has_many :socials
   has_many :events
-  belongs_to :category_country
+  has_many :category_countries, through: :socials
 end
