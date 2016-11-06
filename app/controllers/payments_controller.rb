791039459077865762d9088cf09085db518ce3d2
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-11-06 12:27:57 +0000

    updated

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -23,7 +23,52 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    @payment = Payment.new(payment_params)
+    @subscription_id = params[:subscription_id]
+    @event_id = params[:event_id]
+    
+    # ------- PAYMENT_SUBSCRIPTION -------
+    if @subscription_id.present?
+      @userr = current_userr
+      @payment = Payment.new(payment_params)
+      @subscription = @payment.subscription
+      @payment.user_id = current_user.id
+
+      if current_user.stripe_id?
+        customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      else
+        customer = Stripe::Customer.create(email: current_user.email)
+      end
+
+      payment = customer.subscriptions.create(
+        source: params[:stripeToken],
+        plan: @subscription.id
+        # the subscription.id is the database id
+        # the plan.id in stripe uses the same id as that of the subscription.id in the database in order to select the right subsciption in stripe
+      )
+
+      current_userr.update(
+        stripe_id: customer.id,
+        stripe_payment_id: payment.id,
+        card_last4: params[:card_last4],
+        card_exp_month: params[:card_exp_month],
+        card_exp_year: params[:card_exp_year],
+        card_type: params[:card_brand],
+        recent_subscription_pymt_date: DateTime.now
+      )
+
+    # ------- PAYMENT_EVENT -------
+    elsif @event_id.present?
+      @userr = current_userr
+      @payment = Payment.new(payment_params)
+      @event = @payment.event
+      @payment.user_id = current_user.id
+    end
+
+
+
+
+
+
 
     respond_to do |format|
       if @payment.save
