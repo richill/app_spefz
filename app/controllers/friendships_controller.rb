effc757e6112c6ee6d47d7ca6f47742cb924be83
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-11-08 16:56:39 +0000

    updated

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,20 +2,30 @@ class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @user = User.find(params[:friended_id])
-    current_user.friend(@user)
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
+    if current_user.subscribed_access?
+      @user = User.find(params[:friended_id])
+      current_user.friend(@user)
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
+    else
+      @premium_plan = Subscription.find_by(title:"premium") 
+      redirect_to subscription_path(@premium_plan)
     end
   end
 
   def destroy
-    @user = Friendship.find(params[:id]).friended
-    current_user.unfriend(@user)
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
+    if current_user.subscribed_access?
+      @user = Friendship.find(params[:id]).friended
+      current_user.unfriend(@user)
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
     end
+  else
+    @premium_plan = Subscription.find_by(title:"premium") 
+    redirect_to subscription_path(@premium_plan)
   end
 end
