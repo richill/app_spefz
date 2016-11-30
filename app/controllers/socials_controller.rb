aae5724d82d7f2a764bba38243c2a0786b4dd0b7
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-11-30 23:37:43 +0000

    updated

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -33,8 +33,18 @@ class SocialsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
-    @social = @user.socials.build
+    @premium_plan = Subscription.find_by(title:"premium")
+    if current_user.subscribed_access?
+      if current_user.image?
+        @user = User.find(params[:user_id])
+        @social = @user.socials.build
+      else
+        redirect_to image_restriction_page_path
+      end
+    else
+      @premium_plan = Subscription.find_by(title:"premium")
+      redirect_to subscription_path(@premium_plan)
+    end
   end
 
   def edit
