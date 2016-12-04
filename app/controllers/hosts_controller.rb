117ce61d5c1320fd41c986048813c2ff3e75061a
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-04 21:36:49 +0000

    updated

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,28 +1,20 @@
 class HostsController < ApplicationController
   before_action :set_host, only: [:show, :edit, :update, :destroy]
 
-  # GET /hosts
-  # GET /hosts.json
   def index
     @hosts = Host.all
   end
 
-  # GET /hosts/1
-  # GET /hosts/1.json
   def show
   end
 
-  # GET /hosts/new
   def new
     @host = Host.new
   end
 
-  # GET /hosts/1/edit
   def edit
   end
 
-  # POST /hosts
-  # POST /hosts.json
   def create
     @host = Host.new(host_params)
 
@@ -37,8 +29,6 @@ class HostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /hosts/1
-  # PATCH/PUT /hosts/1.json
   def update
     respond_to do |format|
       if @host.update(host_params)
@@ -51,8 +41,6 @@ class HostsController < ApplicationController
     end
   end
 
-  # DELETE /hosts/1
-  # DELETE /hosts/1.json
   def destroy
     @host.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class HostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_host
       @host = Host.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def host_params
       params.require(:host).permit(:event_id, user_ids: [])
     end
