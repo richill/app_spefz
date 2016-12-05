ecd9d7f2d25ae62dec6973900d2700f4e80eb2f1
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-05 13:52:37 +0000

    updated

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category_blog
 end
--- app/models/category_blog.rb
+++ app/models/category_blog.rb
@@ -1,3 +1,3 @@
 class CategoryBlog < ActiveRecord::Base
-  belongs_to :user
+  has_many :blogs
 end
