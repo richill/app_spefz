423034da018493520784a8ae21c905f99b966d9a
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-15 14:02:35 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215131236) do
+ActiveRecord::Schema.define(version: 20161215131457) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -465,10 +465,12 @@ ActiveRecord::Schema.define(version: 20161215131236) do
     t.datetime "recent_event_pymt_date"
     t.string   "reference"
     t.text     "host_description"
+    t.string   "slug"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
