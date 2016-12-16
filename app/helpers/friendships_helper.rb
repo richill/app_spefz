5185c29355281e52bda4b9d2206eb645172c7f40
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-16 14:19:03 +0000

    updated

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,15 @@
 module FriendshipsHelper
+  # Return an appropriate friendship status message.
+  def friendship_status(user, friend)
+    friendship = Friendship.find_by_user_id_and_friend_id(user.id, friend.id)
+    return "#{friend.firstname} is not your friend (yet)." if friendship.nil?
+    case friendship.status
+    when 'requested'
+      "#{friend.firstname} would like to be your friend."
+    when 'pending'
+      "You have requested friendship with #{friend.firstname}."
+    when 'accepted'
+      "#{friend.firstname} is your friend."
+    end
+  end
 end
