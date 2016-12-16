d85fcdb67a3b74ca5c76a90986c3be85f187c84f
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-16 12:35:14 +0000

    updated

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,10 @@
 class Friendship < ActiveRecord::Base
+  belongs_to :User
+  belongs_to :friend, class_name: "User", foreign_key: "friend_id"
+  validates_presence_of :user_id, :friend_id
+
   belongs_to :friender, class_name: "User"
   belongs_to :friended, class_name: "User"
-
   validates :friender_id, presence: true
   validates :friended_id, presence: true
 end
