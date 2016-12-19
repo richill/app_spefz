321e2c2e48ea988bb5612a8954f9de149a21379e
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-19 14:35:54 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
 
   has_many :payments
   has_many :blogs
+  has_many :invites, dependent: :destroy
   has_many :events, dependent: :destroy
   has_many :socials, dependent: :destroy
   has_many :favourites, dependent: :destroy
