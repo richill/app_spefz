73b7abaeaecdcc5041bd22be9464f4c2eeeed2c3
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-19 15:19:37 +0000

    updated

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,2 +1,7 @@
 module InvitesHelper
+  def sociallist_for(inviter, invitee)
+    socials = Set.new(Social.where(user: inviter).to_a)
+    sent_socials = Set.new(Invite.where(user: inviter, invitee: invitee).to_a.map(&:social))
+    socials.difference(sent_socials)
+  end
 end
