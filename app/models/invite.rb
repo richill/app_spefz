5cf0e1892459c71e42514c6d5b8d6226c42396d5
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-19 14:34:35 +0000

    updated

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,15 @@
 class Invite < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :social
+  belongs_to :event
+  belongs_to :invitee, class_name: "User", foreign_key: "invitee_id"
+  validates_presence_of :user_id, :invitee_id
+
+  def self.exists?(user, invitee)
+    not find_by_user_id_and_invitee_id(user.id, invitee.id).nil?
+  end
 end
+
+
+
+
