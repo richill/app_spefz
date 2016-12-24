582befb2251cd5e48ea6588b2752c5fd6a435885
Richill Tamakloe <richill.tamakloe@gmail.com>
2016-12-24 20:41:38 +0100

    updated

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,13 @@ class Report < ActiveRecord::Base
   belongs_to :social
   belongs_to :event
   belongs_to :user
+
+  before_create :generate_reference_number
+
+  def generate_reference_number
+    begin
+      reference_length = 6
+      self.reference = "SPz_" + Devise.friendly_token.first(reference_length)
+    end while self.class.exists?(reference: reference)
+  end
 end
