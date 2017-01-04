c20b6042385cb31f739ee20cab64a2343d67f592
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-04 13:18:37 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104120524) do
+ActiveRecord::Schema.define(version: 20170104131815) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170104120524) do
     t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   create_table "category_ages", force: :cascade do |t|
