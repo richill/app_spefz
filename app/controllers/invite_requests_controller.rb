a4e34dcc003bfdad47b9bd5133b5e225bfaa0388
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-06 17:50:56 +0000

    updated

--- app/controllers/invite_requests_controller.rb
+++ app/controllers/invite_requests_controller.rb
@@ -31,6 +31,8 @@ class InviteRequestsController < ApplicationController
   end
 
   def update
+    @invite_request.status = "declined"
+    
     respond_to do |format|
       if @invite_request.update(invite_request_params)
         format.html { redirect_to @invite_request, notice: 'Invite request was successfully updated.' }
