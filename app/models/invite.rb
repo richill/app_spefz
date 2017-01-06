1afe3cf700f746542a73c0cc93cb76bf58852b45
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-06 14:57:29 +0000

    updated

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -5,7 +5,7 @@ class Invite < ActiveRecord::Base
   belongs_to :invitee, class_name: "User", foreign_key: "invitee_id"
   validates_presence_of :user_id, :invitee_id
 
-  scope :invited?, -> (user, social) { where(user_id: user.id, event_id: social.id) }
+  scope :invited?, -> (user, social) { where(user_id: user.id, social_id: social.id) }
 
   def self.exists?(user, invitee)
     not find_by_user_id_and_invitee_id(user.id, invitee.id).nil?
