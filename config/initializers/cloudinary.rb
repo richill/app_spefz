b90daa168f565f21b9c6c7cd9d56e69be8eb25c9
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-12 13:39:01 +0000

    updated

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'africanjober'
+  config.api_key = '779314312381268'
+  config.api_secret = 'yTzH36QeozcV8LUzHJrYIcYT05g'
+  config.cdn_subdomain = true
+end
+
