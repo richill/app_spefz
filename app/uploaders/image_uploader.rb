17276180e8bca22f0a0085e648b879e25fea45c3
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-12 13:26:17 +0000

    updated

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,18 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  process :tags => ['post_picture']
+  
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+  
+  version :thumbnail do
+    resize_to_fit(50, 50)
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
