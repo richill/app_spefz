6ad21abecf47dec0db2e5aa8b87bee3183fdb6ab
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-16 18:16:08 +0000

    updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  def create
-    @user = User.find(params[:id])
-    MailerWelcomemember.welcome_member(@user).deliver
-  end
+  # def create
+  #   @user = User.find(params[:id])
+  #   MailerWelcomemember.welcome_member(@user).deliver
+  # end
 
   protected
     def after_update_path_for(resource)
