5d4a9f760ea0f78aed17f0817a126e73828d28cc
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-17 14:06:55 +0000

    updated

--- app/mailers/mailer_report.rb
+++ app/mailers/mailer_report.rb
@@ -3,6 +3,6 @@ class MailerReport < ApplicationMailer
     @report = report
     mail( to: "info@spefz.com",
           subject: "Spefz | <Report> Ref: #{report.reference} - #{report.category_report.name}", 
-          from: "<no-reply@africanjober.com>")
+          from: "<no-reply@spefz.com>")
   end
 end
