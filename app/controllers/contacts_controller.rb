ff6edefb384c7abb4d90617e07a534e5c3c9493f
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-17 15:53:02 +0000

    updated

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,29 +2,21 @@ class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
   before_filter :setup_friends, :setup_subscription, :setup_cards, :setup_events, :setup_invite_form, :setup_user_network_activities
 
-  # GET /contacts
-  # GET /contacts.json
   def index
     @contacts = Contact.all
   end
 
-  # GET /contacts/1
-  # GET /contacts/1.json
   def show
   end
 
-  # GET /contacts/new
   def new
     @premium_plan = Subscription.find_by(title:"premium")
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
   def edit
   end
 
-  # POST /contacts
-  # POST /contacts.json
   def create
     @contact = Contact.new(contact_params)
 
@@ -40,8 +32,6 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
   def update
     respond_to do |format|
       if @contact.update(contact_params)
@@ -54,8 +44,6 @@ class ContactsController < ApplicationController
     end
   end
 
-  # DELETE /contacts/1
-  # DELETE /contacts/1.json
   def destroy
     @contact.destroy
     respond_to do |format|
@@ -95,12 +83,10 @@ class ContactsController < ApplicationController
     end
   end
   
-  # Use callbacks to share common setup or constraints between actions.
   def set_contact
     @contact = Contact.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def contact_params
     params.require(:contact).permit(:email, :message, :category_enquiry_id, :tel, :status, :reference, :category_contactclient_id, :lastname, :companyname, :firstname)
   end
