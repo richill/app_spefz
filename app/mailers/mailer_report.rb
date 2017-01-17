82f86f71177cd05fc2365171b391a9b8c1d092ec
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-17 13:11:55 +0000

    updated

--- app/mailers/mailer_report.rb
+++ app/mailers/mailer_report.rb
@@ -1,2 +1,8 @@
 class MailerReport < ApplicationMailer
+  def report_content(user)
+    @user = user
+    mail( to: "#{user.email}", 
+          subject: "Spefz | Welcome to SPefz!", 
+          from: "<no-reply@spefz.com>")
+  end
 end
