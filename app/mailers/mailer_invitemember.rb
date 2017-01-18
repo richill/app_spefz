355501c83ccd8badf153a1d5af4790f88ece6a16
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-18 11:50:52 +0000

    updated

--- app/mailers/mailer_invitemember.rb
+++ app/mailers/mailer_invitemember.rb
@@ -1,9 +1,8 @@
 class MailerInvitemember < ApplicationMailer
-  def invitation(user, friend)
-    @user = user
-    @friend = friend
-    mail( to: "#{friend.email}", 
-          subject: "Spefz | Private Invite to New Members Night Out (West London)", 
+  def invitation(invite)
+    @invite = invite
+    mail( to: "#{invite.invitee.email}", 
+          subject: "Spefz | Private Invite to #{invite.social.title}", 
           from: "<no-reply@spefz.com>")
   end
 end
