7045f9c0d848c13d3fa02a18fdc7303ff98f5b98
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-18 13:35:23 +0000

    updated

--- app/mailers/mailer_paymentreceipt.rb
+++
@@ -1,8 +0,0 @@
-class MailerPaymentreceipt < ApplicationMailer
-  def userpayment(report)
-    @report = report
-    mail( to: "info@spefz.com",
-          subject: "Spefz | <Report> Ref: #{report.reference} - #{report.category_report.name}", 
-          from: "<no-reply@spefz.com>")
-  end
-end
