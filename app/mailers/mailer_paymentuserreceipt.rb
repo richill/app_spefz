6f41b29fd1c71e32caee1fa89c282838835ebbe8
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-18 13:54:16 +0000

    updated

--- app/mailers/mailer_paymentuserreceipt.rb
+++ app/mailers/mailer_paymentuserreceipt.rb
@@ -1,2 +1,8 @@
 class MailerPaymentuserreceipt < ApplicationMailer
+  def paymentreceipt(@payment)
+    @payment = payment
+    mail( to: "#{payment.email}",
+          subject: "Spefz | Your Spefz Receipt", 
+          from: "<no-reply@spefz.com>")
+  end
 end
