020db408f2ad9ac6953f7631616290951f5550b3
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-18 11:40:39 +0000

    updated

---
+++ app/mailers/mailer_invitemember.rb
@@ -0,0 +1,2 @@
+class MailerInvitemember < ApplicationMailer
+end
---
+++ test/mailers/previews/mailer_invitemember_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/mailer_invitemember
+class MailerInvitememberPreview < ActionMailer::Preview
+
+end
