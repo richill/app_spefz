bf77e58f8051adfe87f040d643e7b3b136b94f67
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-19 23:40:31 +0000

    updated

--- app/models/event.rb
+++ app/models/event.rb
@@ -68,6 +68,25 @@ class Event < ActiveRecord::Base
     self.event_score_access.to_f.round(1) <= user.overall_ratings
   end
 
+  # def match(user)
+  #   # user_rating:  3  | user_rating_percentage:  (user_rating/5)*100:  60%
+  #   # event_access: 2  | event_access_percentage: (event_access/5)*100: 50%
+  #   # match: (event_access_percentage/user_rating_percentage)*100:      83.3%
+  #   # match.round(0) = 83%
+  #   if self.event_score_access.blank?
+  #     if self.event_score_access.blank?
+  #       event_access = self.event_score_access = user.overall_ratings
+  #     end
+  #   end
+  #   user_rating = user.overall_ratings
+  #   user_rating_percentage = (user_rating/5)*100
+  #   event_access = self.event_score_access.to_f
+  #   event_access_percentage = (event_access/5)*100
+  #   match = (event_access_percentage/user_rating_percentage)*100 
+  #   match.round(0)
+  # end
+
+
   def match(user)
     # user_rating:  3  | user_rating_percentage:  (user_rating/5)*100:  60%
     # event_access: 2  | event_access_percentage: (event_access/5)*100: 50%
@@ -83,7 +102,11 @@ class Event < ActiveRecord::Base
     event_access = self.event_score_access.to_f
     event_access_percentage = (event_access/5)*100
     match = (event_access_percentage/user_rating_percentage)*100 
-    match.round(0)
+    if match.is_a?(Float) && match.nan?
+      100
+    else
+      match_result = match.round(0)
+    end
   end
 
   def generate_reference_number
