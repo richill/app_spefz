5c26c9071c5e94db88efd4721966fbc34c60b9e9
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-27 13:35:12 +0000

    updated

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,7 +3,7 @@ class Attendance < ActiveRecord::Base
   tracked only: [:create], owner: -> (controller, model) { controller && controller.current_user }
 
   belongs_to :user
-  belongs_to :attendable, polymorphic: true
+  belongs_to :attendable, polymorphic: true, unique: true
 
   scope :female_attendance, -> { joins(user: :category_gender).where('category_genders.name' => "Female") }
   scope :male_attendance, -> { joins(user: :category_gender).where('category_genders.name' => "Male") }
--- db/migrate/20170127132020_add_uniq_to_attendances.rb
+++ db/migrate/20170127132020_add_uniq_to_attendances.rb
@@ -1,5 +1,6 @@
 class AddUniqToAttendances < ActiveRecord::Migration
   def change
+    add_index :users, :email, unique: true
     add_index :attendances, :attendable_id, unique: true
     add_index :attendances, :attendable_type, unique: true
   end
