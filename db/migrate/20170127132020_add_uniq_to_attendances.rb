9eca8beb0fc79d41562a7b07ba40a23babb001de
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-27 13:35:33 +0000

    updated

--- db/migrate/20170127132020_add_uniq_to_attendances.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqToAttendances < ActiveRecord::Migration
-  def change
-    add_index :users, :email, unique: true
-    add_index :attendances, :attendable_id, unique: true
-    add_index :attendances, :attendable_type, unique: true
-  end
-end
