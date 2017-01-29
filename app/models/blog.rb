fff39af94ae43f91ae16ec84007bf0414c89af11
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-29 14:43:39 +0000

    updated

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,14 @@ class Blog < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug_blogs, use: :slugged
 
+  validates_presence_of :title, presence: true, message: "can't be blank"
+  validates_presence_of :content, presence: true, message: "can't be blank"
+  validates_presence_of :user_id, presence: true, message: "can't be blank"
+  validates_presence_of :category_blog_id, presence: true, message: "can't be blank"
+  validates_presence_of :user_link, presence: true, message: "can't be blank"
+  validates_presence_of :twitter_link, presence: true, message: "can't be blank"
+  validate :image_or_blogimagelink_or_videolink
+
   mount_uploader :image, ImageUploader
   belongs_to :user
   belongs_to :category_blog
@@ -15,5 +23,13 @@ class Blog < ActiveRecord::Base
     [
       :title
     ]
+  end
+
+  def image_or_blogimagelink_or_videolink
+    if image.blank? ^ blog_image_link.blank? ^ video_link.blank?
+      errors.add(:image, "either upload an Image, paste an image link or video link")
+      errors.add(:blog_image_link, "either upload an Image, paste an image link or video link")
+      errors.add(:video_link, "either upload an Image, paste an image link or video link")
+    end
   end
 end
