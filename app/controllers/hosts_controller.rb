0f15d3c2404d437fdb6fecb2c0af05fd67fc82a1
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-30 17:10:26 +0000

    updated

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,59 +1,87 @@
 class HostsController < ApplicationController
-  before_action :set_host, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :set_host, only: [:show, :edit, :update, :destroy]
 
   def index
-    @hosts = Host.all
+    if current_user.admin_pa_management_group 
+      @hosts = Host.all
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def show
+    if current_user.admin_pa_management_group
+    unless current_user.admin_pa_management_group
+      redirect_to errorpermission_path
+    end
   end
 
   def new
-    @event = Event.friendly.find(params[:event_id])
-    @host = @event.hosts.build
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @host = @event.hosts.build
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def edit
-    @event = Event.friendly.find(params[:event_id])
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def create
-    @event = Event.friendly.find(params[:event_id])
-    @event.host = Host.new(host_params)
-    @host = @event.host
-    respond_to do |format|
-      if @host.save
-        format.html { redirect_to(event_path(@event), notice: 'Host was successfully created.') }
-        format.json { render :show, status: :created, location: @host }
-      else
-        format.html { redirect_to :back, alert: 'Hosts for the event was not created.' }
-        format.json { render json: @host.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @event.host = Host.new(host_params)
+      @host = @event.host
+      respond_to do |format|
+        if @host.save
+          format.html { redirect_to(event_path(@event), notice: 'Host was successfully created.') }
+          format.json { render :show, status: :created, location: @host }
+        else
+          format.html { redirect_to :back, alert: 'Hosts for the event was not created.' }
+          format.json { render json: @host.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def update
-    @event = Event.friendly.find(params[:event_id])
-    @host = @event.host
-    respond_to do |format|
-      if @host.update(host_params)
-        format.html { redirect_to(event_path(@event), notice: 'Host was successfully updated.') }
-        format.html { redirect_to([@host.event, @host], notice: 'Host was successfully updated.') }
-        format.json { render :show, status: :ok, location: @host }
-      else
-        format.html { render :edit }
-        format.json { render json: @host.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @host = @event.host
+      respond_to do |format|
+        if @host.update(host_params)
+          format.html { redirect_to(event_path(@event), notice: 'Host was successfully updated.') }
+          format.html { redirect_to([@host.event, @host], notice: 'Host was successfully updated.') }
+          format.json { render :show, status: :ok, location: @host }
+        else
+          format.html { render :edit }
+          format.json { render json: @host.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def destroy
-    @event = Event.friendly.find(params[:event_id])
-    @host = @event.hosts.find(params[:id])
-    @host.destroy
-    respond_to do |format|
-      format.html { redirect_to hosts_url, notice: 'Host was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user.admin_pa_management_group
+      @event = Event.friendly.find(params[:event_id])
+      @host = @event.hosts.find(params[:id])
+      @host.destroy
+      respond_to do |format|
+        format.html { redirect_to hosts_url, notice: 'Host was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to errorpermission_path
     end
   end
 
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,65 +1,91 @@
 class SubscriptionsController < ApplicationController
-  before_action :set_subscription, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :set_subscription, only: [:show, :edit, :update, :destroy]
   before_filter :setup_friends, :setup_subscription, :setup_cards, :setup_events, :setup_invite_form, :setup_user_network_activities
 
   # GET /subscriptions
   # GET /subscriptions.json
   def index
-    @subscriptions = Subscription.all
+    if current_user.admin_pa_management_group
+      @subscriptions = Subscription.all
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   # GET /subscriptions/1
   # GET /subscriptions/1.json
   def show
-    @premium_plan = Subscription.find_by(title:"premium")
+    if current_user == @user
+      @premium_plan = Subscription.find_by(title:"premium")
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   # GET /subscriptions/new
   def new
-    @subscription = Subscription.new
+    if current_user.admin_pa_management_group
+      @subscription = Subscription.new
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   # GET /subscriptions/1/edit
   def edit
+    unless current_user.admin_pa_management_group
+      redirect_to errorpermission_path
+    end
   end
 
   # POST /subscriptions
   # POST /subscriptions.json
   def create
-    @subscription = Subscription.new(subscription_params)
-
-    respond_to do |format|
-      if @subscription.save
-        format.html { redirect_to @subscription, notice: 'Subscription was successfully created.' }
-        format.json { render :show, status: :created, location: @subscription }
-      else
-        format.html { render :new }
-        format.json { render json: @subscription.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group
+      @subscription = Subscription.new(subscription_params)
+      respond_to do |format|
+        if @subscription.save
+          format.html { redirect_to @subscription, notice: 'Subscription was successfully created.' }
+          format.json { render :show, status: :created, location: @subscription }
+        else
+          format.html { render :new }
+          format.json { render json: @subscription.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   # PATCH/PUT /subscriptions/1
   # PATCH/PUT /subscriptions/1.json
   def update
-    respond_to do |format|
-      if @subscription.update(subscription_params)
-        format.html { redirect_to @subscription, notice: 'Subscription was successfully updated.' }
-        format.json { render :show, status: :ok, location: @subscription }
-      else
-        format.html { render :edit }
-        format.json { render json: @subscription.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group
+      respond_to do |format|
+        if @subscription.update(subscription_params)
+          format.html { redirect_to @subscription, notice: 'Subscription was successfully updated.' }
+          format.json { render :show, status: :ok, location: @subscription }
+        else
+          format.html { render :edit }
+          format.json { render json: @subscription.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   # DELETE /subscriptions/1
   # DELETE /subscriptions/1.json
   def destroy
-    @subscription.destroy
-    respond_to do |format|
-      format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user.admin_pa_management_group
+      @subscription.destroy
+      respond_to do |format|
+        format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to errorpermission_path
     end
   end
 
