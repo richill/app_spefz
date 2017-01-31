50ef5d65e18a6a27cf3a44a5d6821fb32de957df
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-01-31 11:55:59 +0000

    updated

--- app/models/category_country.rb
+++ app/models/category_country.rb
@@ -1,5 +1,6 @@
 class CategoryCountry < ActiveRecord::Base
   has_many :socials
   has_many :events
+  has_many :users
   has_many :category_topics, through: :socials
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
   belongs_to :category_role 
   belongs_to :category_managementgroup 
   belongs_to :category_inviteoption 
+  belongs_to :category_country 
   
   has_and_belongs_to_many :hosts
   has_and_belongs_to_many :socials 
