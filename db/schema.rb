87734f8d5407e6fd65e85316e1604e4eedcd9090
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-01 16:45:28 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201160237) do
+ActiveRecord::Schema.define(version: 20170201164503) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -610,6 +610,9 @@ ActiveRecord::Schema.define(version: 20170201160237) do
     t.integer  "category_country_id"
     t.string   "city"
     t.text     "note"
+    t.string   "slug"
   end
+
+  add_index "venues", ["slug"], name: "index_venues_on_slug", unique: true
 
 end
