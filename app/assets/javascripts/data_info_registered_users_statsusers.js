38eed5b0885a1497957129749b009189562e277a
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-07 14:49:57 +0000

    updated

---
+++ app/assets/javascripts/data_info_registered_users_statsusers.js
@@ -0,0 +1,44 @@
+/*=========================================
+    Userrs admin overview
+    File: views / shared / _data_info_registered_users.html.erb
+  =========================================*/
+
+$(document).ready(function() {
+  $('#status_statsusers_all').click(function() {
+    $("#status_statsusers_all_content").show();
+    $("#status_statsusers_male_content").hide();
+    $("#status_statsusers_female_content").hide();
+    $("#status_statsusers_rated_content").hide();
+    $("#status_statsusers_nonrated_content").hide();
+  });
+
+  $('#status_statsusers_male').click(function() {
+    $("#status_statsusers_male_content").show();
+    $("#status_statsusers_all_content").hide();
+    $("#status_statsusers_female_content").hide();
+    $("#status_statsusers_rated_content").hide();
+    $("#status_statsusers_nonrated_content").hide();
+  });
+
+  $('#status_statsusers_female').click(function() {
+    $("#status_statsusers_female_content").show();
+    $("#status_statsusers_all_content").hide();
+    $("#status_statsusers_male_content").hide();
+    $("#status_statsusers_rated_content").hide();
+    $("#status_statsusers_nonrated_content").hide();
+  });
+  $('#status_statsusers_rated').click(function() {
+    $("#status_statsusers_rated_content").show();
+    $("#status_statsusers_all_content").hide();
+    $("#status_statsusers_male_content").hide();
+    $("#status_statsusers_female_content").hide();
+    $("#status_statsusers_nonrated_content").hide();
+  });
+  $('#status_statsusers_nonrated').click(function() {
+    $("#status_statsusers_nonrated_content").show();
+    $("#status_statsusers_all_content").hide();
+    $("#status_statsusers_male_content").hide();
+    $("#status_statsusers_female_content").hide();
+    $("#status_statsusers_rated_content").hide();
+  });
+});
