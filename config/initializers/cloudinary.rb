6af4814e5e477a0105e55b82e267af99b4097552
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-08 13:21:11 +0000

    updated

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,15 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'africanjober'
-  config.api_key = '779314312381268'
-  config.api_secret = 'yTzH36QeozcV8LUzHJrYIcYT05g'
+  config.cloud_name = ENV["SP_CLOUDINARY_CLOUDNAME"]
+  config.api_key = ENV["SP_CLOUDINARY_API_KEY"]
+  config.api_secret = ENV["SP_CLOUDINARY_API_SECRET"]
   config.cdn_subdomain = true
 end
+
+# Cloudinary.config do |config|
+#   config.cloud_name = 'africanjober'
+#   config.api_key = '779314312381268'
+#   config.api_secret = 'yTzH36QeozcV8LUzHJrYIcYT05g'
+#   config.cdn_subdomain = true
+# end
 
 
