168f03b02c5d3e3f09c40251e8098cc400aa0470
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-08 16:13:08 +0000

    updated

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
   config.cloud_name = ENV["SP_CLOUDINARY_CLOUDNAME"]
   config.api_key = ENV["SP_CLOUDINARY_API_KEY"]
-  config.api_secret = ENV["SP_CLOUDINARY_API_SECRET"]
+  config.api_secret = ENV["SP_CLOUDINARY_API_SECRET_KEY"]
   config.cdn_subdomain = true
 end
 
