77a0dc5e4b279fb4d5d90733d239b74649e3d076
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-10 21:21:35 +0000

    updated

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,17 +1,19 @@
-# production
-Cloudinary.config do |config|
-  config.cloud_name = ENV["CLOUDINARY_CLOUDNAME"]
-  config.api_key = ENV["CLOUDINARY_API_KEY"]
-  config.api_secret = ENV["CLOUDINARY_API_SECRET_KEY"]
-  config.cdn_subdomain = true
+if Rails.env.development?
+  #development
+  Cloudinary.config do |config|
+    config.cloud_name = ENV["SP_CLOUDINARY_CLOUDNAME"]
+    config.api_key = ENV["SP_CLOUDINARY_API_KEY"]
+    config.api_secret = ENV["SP_CLOUDINARY_API_SECRET_KEY"]
+    config.cdn_subdomain = true
+  end
+elsif Rails.env.production?
+  # production
+  Cloudinary.config do |config|
+    config.cloud_name = ENV["CLOUDINARY_CLOUDNAME"]
+    config.api_key = ENV["CLOUDINARY_API_KEY"]
+    config.api_secret = ENV["CLOUDINARY_API_SECRET_KEY"]
+    config.cdn_subdomain = true
+  end
 end
-
-# development
-# Cloudinary.config do |config|
-#   config.cloud_name = ENV["SP_CLOUDINARY_CLOUDNAME"]
-#   config.api_key = ENV["SP_CLOUDINARY_API_KEY"]
-#   config.api_secret = ENV["SP_CLOUDINARY_API_SECRET_KEY"]
-#   config.cdn_subdomain = true
-# end
 
 
