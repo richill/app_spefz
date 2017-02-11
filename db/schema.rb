27870b2b75a9dd6ddb475d019900400c99fd20ba
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-11 15:58:27 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -246,9 +246,6 @@ ActiveRecord::Schema.define(version: 20170211154827) do
   create_table "friendships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.string   "status"
   end
 
   create_table "hosts", force: :cascade do |t|
