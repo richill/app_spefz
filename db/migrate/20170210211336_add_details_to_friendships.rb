5911c0c6e6bc2d51e2dc709933ea05c5754380b6
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-11 15:56:45 +0000

    updated

--- db/migrate/20170210211336_add_details_to_friendships.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToFriendships < ActiveRecord::Migration
-  def change
-    add_column :friendships, :user_id, :integer
-    add_column :friendships, :friend_id, :integer
-    add_column :friendships, :status, :string
-  end
-end
