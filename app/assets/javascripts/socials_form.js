9f4a87941f23108815505d320acd8e5523a928d1
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-02-16 11:35:13 +0000

    updated

--- app/assets/javascripts/socials_form.js
+++ app/assets/javascripts/socials_form.js
@@ -4,10 +4,24 @@
   =========================================*/
 
 // filter on gender attendance
+// $(document).ready(function() {
+//   $('.general_gender_quantity_content').hide();
+//   $('.specific_gender_quantity_content').hide();
+//   $('#social_category_quantitygender_id').change(function() {
+//     if ($(this).val() == 4) {
+//       $('.specific_gender_quantity_content').show();
+//       $('.general_gender_quantity_content').hide();
+//     } else if ($(this).val() == 1, 2, 3) {
+//       $('.general_gender_quantity_content').show();
+//       $('.specific_gender_quantity_content').hide();
+//     }
+//   });
+// });
+
 $(document).ready(function() {
   $('.general_gender_quantity_content').hide();
   $('.specific_gender_quantity_content').hide();
-  $('#social_category_quantitygender_id').change(function() {
+  $('#social_category_quantitygendersocial_id').change(function() {
     if ($(this).val() == 4) {
       $('.specific_gender_quantity_content').show();
       $('.general_gender_quantity_content').hide();
@@ -20,7 +34,8 @@ $(document).ready(function() {
 
 
 // hiding the option 'both X men & X women' in the category 'CategoryQuantitygender'
-$(document).ready(function() {
-  $('#social_category_quantitygender_id').find("option[value='4']").addClass("hide")
-});
+// $(document).ready(function() {
+//   $('#social_category_quantitygender_id').find("option[value='4']").addClass("hide")
+// });
+
 
