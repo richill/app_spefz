818b3022ce2e9d1ccbd045561b7cef57bf69c283
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-03 14:02:15 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   extend FriendlyId
   friendly_id :slug_users, use: :slugged
@@ -366,5 +366,11 @@ class User < ActiveRecord::Base
       return false
     end
   end
+
+  protected
+  # deactivates confirmable
+  # def confirmation_required?
+  #   false
+  # end
 end
 
