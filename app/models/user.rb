f90013ad282104515b4740428536a2672bad55b3
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-07 15:14:39 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -372,5 +372,10 @@ class User < ActiveRecord::Base
   # def confirmation_required?
   #   false
   # end
+  def confirmation_required?
+    if Rails.env.development?
+      false
+    end
+  end
 end
 
