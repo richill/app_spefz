55153065f836825389fe4d77d4988c02c18c98ba
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-14 12:18:40 +0000

    updated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -147,6 +147,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :description, :address, :postcode, :latitude, :longitude, :user_id, :category_topic_id, :image, :date, :time, :city, :quantity, :category_age_id, :category_country_id, :quantity_men, :quantity_women, :category_quantitygender_id, :venuename, :price, :time_end, :tag_list, :maplink, :close, :slug, :event_score_access, :display_match, :upcoming_event)
+    params.require(:event).permit(:title, :description, :address, :postcode, :latitude, :longitude, :user_id, :category_topic_id, :image, :date, :time, :city, :quantity, :category_age_id, :category_country_id, :quantity_men, :quantity_women, :category_quantitygender_id, :venuename, :price, :time_end, :tag_list, :maplink, :close, :slug, :event_score_access, :display_match, :upcoming_event, :agerange_info, :agerange)
   end
 end
