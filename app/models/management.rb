7842fb046d13cca8481bbdf1b9a4dcefb031935c
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-17 22:58:13 +0000

    updated

--- app/models/management.rb
+++ app/models/management.rb
@@ -1,3 +1,27 @@
 class Management < ActiveRecord::Base
   belongs_to :event
+
+  def ticket_sales
+    self.event.payments.count * self.event.price
+  end
+
+  def cost_host
+    self.event.host.users.count * 50
+  end
+
+  def vat
+    ticket_sales * 0.20
+  end
+
+  def revenue
+    ticket_sales - (self.venuecost + cost_host + vat)
+  end
+
+  def deposit
+    1000
+  end
+
+  def profit
+    revenue - deposit
+  end
 end
