012d66c79920be92185a9daf9c51d9b023e21367
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-06-20 21:37:33 +0100

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -378,7 +378,7 @@ class User < ActiveRecord::Base
 
   def self.platform_sufficient_users
     users = User.all
-    users.females.count > 1 && users.males.count > 1
+    users.females.count > 0 && users.males.count > 0
   end
 
   def matching(other_user)
