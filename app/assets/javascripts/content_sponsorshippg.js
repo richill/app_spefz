070371a98c0829ebb67cb3e2e0e8905fbe4744ab
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-10-01 16:19:35 +0100

    updated

--- app/assets/javascripts/content_sponsorshippg.js
+++
@@ -1,6 +0,0 @@
-/*=========================================
-    Section: sponsorship page
-    File: views / static pages / sponsorshippg.html.erb
-  =========================================*/
-
-// filter sponsorship sections
