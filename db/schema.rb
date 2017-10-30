42daefed79636b998073e8a20cd0eb539730cde0
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-10-30 20:00:12 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730095848) do
+ActiveRecord::Schema.define(version: 20171030195853) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -734,6 +734,8 @@ ActiveRecord::Schema.define(version: 20170730095848) do
     t.integer  "category_quantitygendersocial_id"
     t.boolean  "upcoming_social"
     t.boolean  "company_image"
+    t.boolean  "eventbrite"
+    t.text     "eventbrite_url"
   end
 
   add_index "socials", ["slug"], name: "index_socials_on_slug", unique: true
