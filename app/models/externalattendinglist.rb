66c1c71459038a7168a5f64fcec62d5928470b66
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-12-26 14:12:21 +0000

    updated

--- app/models/externalattendinglist.rb
+++ app/models/externalattendinglist.rb
@@ -1,4 +1,13 @@
 class Externalattendinglist < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :event
+
+  before_create :generate_reference_number
+
+  def generate_reference_number
+    begin
+      reference_length = 6
+      self.reference = "SPz_" + Devise.friendly_token.first(reference_length)
+    end while self.class.exists?(reference: reference)
+  end
 end
