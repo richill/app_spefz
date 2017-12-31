7238d9e913ac33a54fff3cfc1f55392b7f9336a1
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-12-31 19:33:15 +0000

    updated

--- app/controllers/externalattendinglists_controller.rb
+++ app/controllers/externalattendinglists_controller.rb
@@ -3,109 +3,88 @@ class ExternalattendinglistsController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    @externalattendinglists = Externalattendinglist.all
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @externalattendinglists = Externalattendinglist.all
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def show
+    unless current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      redirect_to errorpermission_path
+    end
   end
 
   def new
-    @event = Event.friendly.find(params[:event_id])
-    @externalattendinglist = Externalattendinglist.new
-    @externalattendinglist.event = @event
-    respond_with(@externalattendinglist)
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @externalattendinglist = Externalattendinglist.new
+      @externalattendinglist.event = @event
+      respond_with(@externalattendinglist)
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def edit
-    @event = Event.friendly.find(params[:event_id])
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+    else
+      redirect_to errorpermission_path
+    end
   end
 
   def create
-    @event = Event.friendly.find(params[:event_id])
-    @event.externalattendinglist = Externalattendinglist.new(externalattendinglist_params)
-    @externalattendinglist = @event.externalattendinglist
-    respond_to do |format|
-      if @externalattendinglist.save   
-        format.html { redirect_to(attendants_event_path(@event), notice: 'External Attending List Successfully Created.') }
-        format.json { render :show, status: :created, location: @externalattendinglist }
-      else
-        format.html { redirect_to :back, alert: 'External Attending List Was Not created.' }
-        format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @event.externalattendinglist = Externalattendinglist.new(externalattendinglist_params)
+      @externalattendinglist = @event.externalattendinglist
+      respond_to do |format|
+        if @externalattendinglist.save   
+          format.html { redirect_to(attendants_event_path(@event), notice: 'External Attending List Successfully Created.') }
+          format.json { render :show, status: :created, location: @externalattendinglist }
+        else
+          format.html { redirect_to :back, alert: 'External Attending List Was Not created.' }
+          format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def update
-    @event = Event.friendly.find(params[:event_id])
-    @externalattendinglist = @event.externalattendinglist
-    respond_to do |format|
-      if @externalattendinglist.update(externalattendinglist_params)
-        format.html { redirect_to(attendants_event_path(@event), notice: 'External Attending List Successfully Updated.') }
-        format.json { render :show, status: :ok, location: @externalattendinglist }
-      else
-        format.html { render :edit }
-        format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @externalattendinglist = @event.externalattendinglist
+      respond_to do |format|
+        if @externalattendinglist.update(externalattendinglist_params)
+          format.html { redirect_to(attendants_event_path(@event), notice: 'External Attending List Successfully Updated.') }
+          format.json { render :show, status: :ok, location: @externalattendinglist }
+        else
+          format.html { render :edit }
+          format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def destroy
-    @event = Event.friendly.find(params[:event_id])
-    @externalattendinglist = @event.externalattendinglists.find(params[:id])
-    @externalattendinglist.destroy
-    respond_to do |format|
-      format.html { redirect_to externalattendinglists_url, notice: 'External Attending List Successfully Destroyed.' }
-      format.json { head :no_content }
+    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+      @event = Event.friendly.find(params[:event_id])
+      @externalattendinglist = @event.externalattendinglists.find(params[:id])
+      @externalattendinglist.destroy
+      respond_to do |format|
+        format.html { redirect_to externalattendinglists_url, notice: 'External Attending List Successfully Destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to errorpermission_path
     end
   end
-
-  # def index
-  #   @externalattendinglists = Externalattendinglist.all
-  # end
-
-  # def show
-  # end
-
-  # def new
-  #   @externalattendinglist = Externalattendinglist.new
-  # end
-
-  # def edit
-  # end
-
-  # def create
-  #   @externalattendinglist = Externalattendinglist.new(externalattendinglist_params)
-
-  #   respond_to do |format|
-  #     if @externalattendinglist.save
-  #       format.html { redirect_to @externalattendinglist, notice: 'Externalattendinglist was successfully created.' }
-  #       format.json { render :show, status: :created, location: @externalattendinglist }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # def update
-  #   respond_to do |format|
-  #     if @externalattendinglist.update(externalattendinglist_params)
-  #       format.html { redirect_to @externalattendinglist, notice: 'Externalattendinglist was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @externalattendinglist }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @externalattendinglist.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # def destroy
-  #   @externalattendinglist.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to externalattendinglists_url, notice: 'Externalattendinglist was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   private
     def set_externalattendinglist
