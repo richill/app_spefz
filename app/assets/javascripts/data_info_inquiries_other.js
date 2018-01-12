a1c1e769595c2b628eb43685a609696fe1d46532
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-01-12 15:12:49 +0000

    updated

---
+++ app/assets/javascripts/data_info_inquiries_other.js
@@ -0,0 +1,67 @@
+/*=========================================
+    Userrs admin overview
+    File: views / shared / _data_info_registered_users.html.erb
+  =========================================*/
+
+$(document).ready(function() {
+  $('#contact_inquiry_all').click(function() {
+    $("#contact_inquiry_all_content").show();
+    $("#contact_inquiry_suggestions_content").hide();
+    $("#contact_inquiry_marketing_content").hide();
+    $("#contact_inquiry_affiliates_content").hide();
+    $("#contact_inquiry_suggestedp_content").hide();
+    $("#contact_inquiry_workwithus_content").hide();
+  });
+
+  $('#contact_inquiry_suggestions').click(function() {
+    $("#contact_inquiry_suggestions_content").show();
+    $("#contact_inquiry_all_content").hide();
+    $("#contact_inquiry_marketing_content").hide();
+    $("#contact_inquiry_affiliates_content").hide();
+    $("#contact_inquiry_suggestedp_content").hide();
+    $("#contact_inquiry_workwithus_content").hide();
+  });
+
+  $('#contact_inquiry_marketing').click(function() {
+    $("#contact_inquiry_marketing_content").show();
+    $("#contact_inquiry_all_content").hide();
+    $("#contact_inquiry_suggestions_content").hide();
+    $("#contact_inquiry_affiliates_content").hide();
+    $("#contact_inquiry_suggestedp_content").hide();
+    $("#contact_inquiry_workwithus_content").hide();
+  });
+
+  $('#contact_inquiry_affiliates').click(function() {
+    $("#contact_inquiry_affiliates_content").show();
+    $("#contact_inquiry_all_content").hide();
+    $("#contact_inquiry_suggestions_content").hide();
+    $("#contact_inquiry_marketing_content").hide();
+    $("#contact_inquiry_suggestedp_content").hide();
+    $("#contact_inquiry_workwithus_content").hide();
+  });
+
+  $('#contact_inquiry_suggestedp').click(function() {
+    $("#contact_inquiry_suggestedp_content").show();
+    $("#contact_inquiry_all_content").hide();
+    $("#contact_inquiry_suggestions_content").hide();
+    $("#contact_inquiry_marketing_content").hide();
+    $("#contact_inquiry_affiliates_content").hide();
+    $("#contact_inquiry_workwithus_content").hide();
+  });
+
+  $('#contact_inquiry_workwithus').click(function() {
+    $("#contact_inquiry_workwithus_content").show();
+    $("#contact_inquiry_all_content").hide();
+    $("#contact_inquiry_suggestions_content").hide();
+    $("#contact_inquiry_marketing_content").hide();
+    $("#contact_inquiry_affiliates_content").hide();
+    $("#contact_inquiry_suggestedp_content").hide();
+  });
+});
+
+
+
+
+
+
+
