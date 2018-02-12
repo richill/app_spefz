0b643c08397deaa9450e2a382c9c07d0f2aa488b
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-02-12 18:45:11 +0000

    updated

---
+++ app/assets/javascripts/data_info_statsalbums_albumphotos.js
@@ -0,0 +1,24 @@
+/*=========================================
+    Userrs admin events
+    File: views / shared / _data_info_statsevents_events.html.erb
+  =========================================*/
+
+$(document).ready(function() {
+  $('#status_statsablbum_allphotos').click(function() {
+    $("#status_statsablbum_allphotos_content").show();
+    $("#status_statsablbum_publishedphotos_content").show();
+    $("#status_statsablbum_unpublishedphotos_content").hide();
+  });
+
+  $('#status_statsablbum_publishedphotos').click(function() {
+    $("#status_statsablbum_publishedphotos_content").show();
+    $("#status_statsablbum_allphotos_content").hide();
+    $("#status_statsablbum_unpublishedphotos_content").hide();
+  });
+
+  $('#status_statsablbum_unpublishedphotos').click(function() {
+    $("#status_statsablbum_unpublishedphotos_content").show();
+    $("#status_statsablbum_allphotos_content").hide();
+    $("#status_statsablbum_publishedphotos_content").hide();
+  });
+});
