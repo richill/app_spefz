442de7bfc307b703bfc957cccba83d058aa8364e
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-02-12 16:26:16 +0000

    updated

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,20 @@
 class Album < ActiveRecord::Base
+  validates_presence_of :name, presence: true, message: "can't be blank"
+  validates_presence_of :date, presence: true, message: "can't be blank"
+  validates_presence_of :description, presence: true, message: "can't be blank"
+  validate :image_or_albumlink
+
   belongs_to :user
   has_many :photos, dependent: :destroy
 
   scope :published_albums, -> { where('publish = ?', true) }
   scope :unpublished_albums, -> { where('publish = ?', false) }
+
+
+  def image_or_albumlink
+    if image.blank? ^ album_image_link.blank?
+      errors.add(:image, "either upload an Image, paste an image link or video link")
+      errors.add(:album_image_link, "either upload an Image, paste an image link or video link")
+    end
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,20 @@
 class Photo < ActiveRecord::Base
+  validates_presence_of :name, presence: true, message: "can't be blank"
+  validates_presence_of :description, presence: true, message: "can't be blank"
+  validates_presence_of :album_id, presence: true, message: "please select a category"
+  validate :image_or_photolink
+  
+
+
   belongs_to :album
 
   scope :published_photos, -> { where('publish = ?', true) }
   scope :unpublished_photos, -> { where('publish = ?', false) }
+
+  def image_or_photolink
+    if image.blank? ^ photo_image_link.blank?
+      errors.add(:image, "either upload an Image, paste an image link or video link")
+      errors.add(:photo_image_link, "either upload an Image, paste an image link or video link")
+    end
+  end
 end
