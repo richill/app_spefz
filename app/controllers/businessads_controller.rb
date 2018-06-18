6cb4c03651ebe98af97be35511ac751be99d0e49
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-18 20:01:18 +0100

    updated

--- app/controllers/businessads_controller.rb
+++ app/controllers/businessads_controller.rb
@@ -3,7 +3,7 @@ class BusinessadsController < ApplicationController
   before_filter :setup_friends, :setup_subscription, :setup_cards, :setup_events, :setup_invite_form, :setup_user_network_activities
 
   def index
-    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+    if current_user.admin_pa_management_group || current_user.pa_administration_group
       @businessads = Businessad.all
     else
       redirect_to errorpermission_path
@@ -11,53 +11,95 @@ class BusinessadsController < ApplicationController
   end
 
   def show
-    unless current_user.admin_pa_management_group || current_user.pa_event_mgt_group
+    unless current_user.admin_pa_management_group || current_user.pa_administration_group
       redirect_to errorpermission_path
     end
   end
 
   def new
-    if current_user.admin_pa_management_group || current_user.pa_event_mgt_group
-      @businessad = Businessad.new
+    @user = User.friendly.find(params[:user_id])
+    if current_user.admin_pa_management_group || current_user.pa_administration_group
+      # @businessad = Businessad.new
+      @businessad = @user.businessads.build
     else
       redirect_to errorpermission_path
     end
   end
 
   def edit
+    @user = User.friendly.find(params[:user_id])
+    unless current_user.admin_pa_management_group || current_user.pa_administration_group
+      redirect_to errorpermission_path
+    end
   end
 
   def create
-    @businessad = Businessad.new(businessad_params)
+    # @businessad = Businessad.new(businessad_params)
+    # respond_to do |format|
+    #   if @businessad.save
+    #     format.html { redirect_to @businessad, notice: 'Businessad was successfully created.' }
+    #     format.json { render :show, status: :created, location: @businessad }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @businessad.errors, status: :unprocessable_entity }
+    #   end
+    # end
 
-    respond_to do |format|
-      if @businessad.save
-        format.html { redirect_to @businessad, notice: 'Businessad was successfully created.' }
-        format.json { render :show, status: :created, location: @businessad }
-      else
-        format.html { render :new }
-        format.json { render json: @businessad.errors, status: :unprocessable_entity }
+    @user = User.friendly.find(params[:user_id])
+    if current_user.admin_pa_management_group || current_user.pa_administration_group
+      @businessad = @user.businessads.create(businessad_params)
+      respond_to do |format|
+        if @businessad.save
+          format.html { redirect_to([@businessad.user, @businessad], notice: 'Advert was successfully created.') }
+          format.json  { render json: @businessad, status: :created, location: @businessad }
+        else
+          format.html { render action: "new" }
+          format.json  { render json: @businessad.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def update
-    respond_to do |format|
-      if @businessad.update(businessad_params)
-        format.html { redirect_to @businessad, notice: 'Businessad was successfully updated.' }
-        format.json { render :show, status: :ok, location: @businessad }
-      else
-        format.html { render :edit }
-        format.json { render json: @businessad.errors, status: :unprocessable_entity }
+    # respond_to do |format|
+    #   if @businessad.update(businessad_params)
+    #     format.html { redirect_to @businessad, notice: 'Businessad was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @businessad }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @businessad.errors, status: :unprocessable_entity }
+    #   end
+    # end
+    if current_user.admin_pa_management_group || current_user.pa_administration_group
+      respond_to do |format|
+        if @businessad.update_attributes(blog_params)
+          format.html { redirect_to([@businessad.user, @businessad], notice: 'Advert was successfully updated.') }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @businessad.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to errorpermission_path
     end
   end
 
   def destroy
-    @businessad.destroy
-    respond_to do |format|
-      format.html { redirect_to businessads_url, notice: 'Businessad was successfully destroyed.' }
-      format.json { head :no_content }
+    # @businessad.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to businessads_url, notice: 'Businessad was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
+    if current_user.admin_pa_management_group || current_user.pa_administration_group
+      @user = User.friendly.find(params[:user_id])
+      @blog = @user.businessads.find(params[:id])
+      @businessad.destroy
+      redirect_to businessads_path
+    else
+      redirect_to errorpermission_path
     end
   end
 
