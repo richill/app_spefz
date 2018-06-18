c08e550e08eaa34b8e6ec4ecac180b4d8d336ec1
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-18 19:42:00 +0100

    scaffold business ads

--- app/models/businessad.rb
+++ app/models/businessad.rb
@@ -1,2 +1,4 @@
 class Businessad < ActiveRecord::Base
+  belongs_to :event
+  belongs_to :category_advert
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -45,6 +45,7 @@ class Event < ActiveRecord::Base
   has_many :payments, dependent: :destroy
   has_many :reports, dependent: :destroy
   has_many :managements
+  has_many :businessads
   
   belongs_to :user
   belongs_to :category_topic
