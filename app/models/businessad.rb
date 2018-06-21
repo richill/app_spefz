d0e648867bf28dc4387e83882f8416b509221709
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-21 14:28:03 +0100

    created ref for adverts

--- app/models/businessad.rb
+++ app/models/businessad.rb
@@ -2,4 +2,13 @@ class Businessad < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   belongs_to :category_advert
+
+  before_create :generate_reference_number
+
+  def generate_reference_number
+    begin
+      reference_length = 6
+      self.reference = "SPz_" + Devise.friendly_token.first(reference_length)
+    end while self.class.exists?(reference: reference)
+  end
 end
