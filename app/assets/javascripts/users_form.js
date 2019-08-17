d4ffbe430de5905923c50557c7616190ba74df5a
Richill Tamakloe <richill.tamakloe@gmail.com>
2019-08-17 15:36:17 +0100

    updated profile image

---
+++ app/assets/javascripts/users_form.js
@@ -0,0 +1,20 @@
+/*=========================================
+    Page: users form page
+    File: views/users/_form.html.erb
+  =========================================*/
+
+// display users uploaded image
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+      $('#imagePreview').hide();
+      $('#imagePreview').fadeIn(650);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function() {
+  readURL(this);
+});
